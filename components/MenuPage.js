99eebccf34bf4d2f31f05af92e35a07a84d4a765
Chinonso Emmanuel <emmanuelchinons9@gmail.com>
2026-05-01 17:20:08 +0100

    fetch external css for menu page from MenuPage.css

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -2,6 +2,15 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super();
     this.root = this.attachShadow({ mode: "open" });
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const res = await fetch("./components/MenuPage.css");
+      const css = await res.text();
+      styles.textContent = css;
+    }
+    loadCSS();
   }
 
   // when the component is attached to the DOM
